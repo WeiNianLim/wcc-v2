ad1aa8628730912292ea1b48bebcd6faf26d4b27
William <weinianlim26@gmail.com>
2019-06-11 19:55:26 -0700

    Updates

---
+++ precache-manifest.c86f5175bc33e90a1e2d3360b595a53c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86228cc676a8bb44bbfa1bff686ab81",
+    "url": "/wcc2/index.html"
+  },
+  {
+    "revision": "c4aee7234987770cc9b1",
+    "url": "/wcc2/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "060a233d00804e38c461",
+    "url": "/wcc2/static/js/2.d80a71ab.chunk.js"
+  },
+  {
+    "revision": "c4aee7234987770cc9b1",
+    "url": "/wcc2/static/js/main.8a420e37.chunk.js"
+  },
+  {
+    "revision": "a28f959fdb849cc41c98",
+    "url": "/wcc2/static/js/runtime~main.3e856306.js"
+  }
+]);
