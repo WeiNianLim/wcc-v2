f21730315c88774c06b1dfda0f39874518ed76e8
William <weinianlim26@gmail.com>
2019-07-31 14:10:37 -0700

    Updates

---
+++ precache-manifest.7f4f632fa014ce2f5c36ef88682878e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8fd24651f68f81c6fd97f200a4ab37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5050cf4695f1ca509778",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fa1827a8a2dddad75876",
+    "url": "/static/js/2.ce881e43.chunk.js"
+  },
+  {
+    "revision": "5050cf4695f1ca509778",
+    "url": "/static/js/main.de006b0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
