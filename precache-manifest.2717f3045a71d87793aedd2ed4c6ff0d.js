ca43087bba0810f3b689df025bb9fc3936b10ff9
William Lim <wlim@acromedia.com>
2021-12-29 17:31:26 -0800

    Updates

--- precache-manifest.2717f3045a71d87793aedd2ed4c6ff0d.js
+++ precache-manifest.2717f3045a71d87793aedd2ed4c6ff0d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1ab099d71266b03014020821880def9",
+    "revision": "35e424dd80eb8fbc537c1383b2ccc92f",
     "url": "/index.html"
   },
   {
-    "revision": "4afd7a522d986488988d",
+    "revision": "675e403b1ca58255c0e1",
     "url": "/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "c20e2f2020fd051b14b7",
-    "url": "/static/js/2.db193edf.chunk.js"
+    "revision": "fa1827a8a2dddad75876",
+    "url": "/static/js/2.ce881e43.chunk.js"
   },
   {
-    "revision": "4afd7a522d986488988d",
-    "url": "/static/js/main.5d8f33a7.chunk.js"
+    "revision": "675e403b1ca58255c0e1",
+    "url": "/static/js/main.38081ba4.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a091b0a8a907ac208907abaaf722b2f2.js"
+  "/precache-manifest.2717f3045a71d87793aedd2ed4c6ff0d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
