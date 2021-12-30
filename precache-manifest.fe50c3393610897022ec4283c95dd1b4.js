ac8c783539e73a4044ca213b716e0b9947621d7d
William Lim <wlim@acromedia.com>
2021-12-29 18:18:57 -0800

    Updates

---
+++ precache-manifest.fe50c3393610897022ec4283c95dd1b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad23d8056c0168f8144a1907a954c64",
+    "url": "/wcc2/index.html"
+  },
+  {
+    "revision": "96ab25176431bc09d017",
+    "url": "/wcc2/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8d96f8f451c947cff70b",
+    "url": "/wcc2/static/js/2.4e454a22.chunk.js"
+  },
+  {
+    "revision": "96ab25176431bc09d017",
+    "url": "/wcc2/static/js/main.39b175c7.chunk.js"
+  },
+  {
+    "revision": "a28f959fdb849cc41c98",
+    "url": "/wcc2/static/js/runtime~main.3e856306.js"
+  }
+]);
